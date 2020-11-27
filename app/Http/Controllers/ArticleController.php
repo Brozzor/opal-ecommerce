74c121dc381da892cd838abebfbe69a10f315852
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-11-27 14:54:34 +0100

    ajout du model article et du controller article

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::All();
+        return view('welcome', compact("articles"));
+    }
+
+}
