34c05c973ece549d744cedc67d11b372508b7f43
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-12-15 13:34:41 +0100

    ajout de l'accès au magasin depuis la page d'acceuil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     <nav id="header" class="fixed w-full z-30 top-0 text-white">
         <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
             <div class="pl-4 flex items-center">
-                <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="">
+                <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="/">
                     OPAL HOME
                 </a>
             </div>
@@ -37,6 +37,9 @@
             <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
                 @auth
                 <ul class="list-reset lg:flex justify-end flex-1 items-center">
+                    <li class="mr-3">
+						<a href="{{ route('magasin') }}" class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">Magasin</a>
+					</li>
                     <a href="{{ route('dashboard') }}"  id="navAction" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                         Mon compte
                         </a>
