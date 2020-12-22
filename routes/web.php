dad98b86bc7306dd8b07668bf56d670ad17d6901
Aya Haddad <aya.haddad@ynov.com>
2020-12-22 16:34:40 +0100

    ajout de la route , du controller pour le systeme de recherche

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/search', ['uses' => 'SearchController@recherche', 'as' => 'search']);
+
